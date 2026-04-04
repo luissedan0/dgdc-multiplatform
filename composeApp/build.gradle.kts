9c3a010651ade565eff1f52e06a4f5624bbeb0e6
luissedan0 <sedanodlr@gmail.com>
2026-04-04 11:52:41 -0400

    Updated version in both platforms

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "com.luissedan0.demetergarterdistanceclub"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
     packaging {
         resources {
